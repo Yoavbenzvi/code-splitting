663de261dc15562a4353a42577f76fe92e28f557
Yoav <yoav.benzvi@gmail.com>
2020-04-20 20:56:23 +0200

    another way to write async components using code splitting

--- src/App.js
+++ src/App.js
@@ -2,25 +2,62 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Page1 from './components/Page1';
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
+import AsyncComponent from './components/AsyncComponent';
+
+class App extends React.Component {
+   constructor() {
+      super()
+
+      this.state = {
+         route: 'page1',
+         component: null
+      }
+   }
+ 
+   onRouteChange = (route) => {
+      this.setState({route})
+      //with code splitting:
+      // if(route === 'page1') {
+      //    this.setState({route})
+      // } else if(route === 'page2') {
+      //    import('./components/Page2').then((Page2) => {this.setState({route, component: Page2.default})})
+      // } else if(route === 'page3') {
+      //    import('./components/Page3').then((Page3) => {this.setState({route, component: Page3.default})})
+      // }
+   }
+
+   render() {
+      // return (
+      //    <div className="App">
+      //       {this.state.route === 'page1' && <Page1 onRouteChange={this.onRouteChange}/>}
+      //       {this.state.route === 'page2' && <Page2 onRouteChange={this.onRouteChange}/>}
+      //       {this.state.route === 'page3' && <Page3 onRouteChange={this.onRouteChange}/>}
+      //    </div>
+      // );
+
+      if(this.state.route === 'page1') {
+         return <Page1 onRouteChange={this.onRouteChange}/>
+      } else if(this.state.route === 'page2') {
+
+         const AsyncPage2 = AsyncComponent(() => import('./components/Page2'))
+         return <AsyncPage2 onRouteChange={this.onRouteChange}/>
+
+      } else if(this.state.route === 'page3') {
+
+         const AsyncPage3 = AsyncComponent(() => import('./components/Page3'))
+         return <AsyncPage3 onRouteChange={this.onRouteChange}/>
+
+      }
+
+      // if(this.state.route === 'page1') {
+      //    return <Page1 onRouteChange={this.onRouteChange} />
+      // } else {
+      //    return <this.state.component onRouteChange={this.onRouteChange} />
+      // }
+   }
 }
 
 export default App;
